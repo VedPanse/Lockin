a05d1896cf62e3d9b9668780b5f54bf59bf29505
VedPanse <panse.ved@yandex.com>
2025-11-12 00:49:02 -0800

    Initial Commit

---
+++ Lockheed/LockheedApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockheedApp.swift
+//  Lockheed
+//
+//  Created by Ved Panse on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LockheedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
